7d048af4747f4235f8e1b701e21606cf44894a84
Aswinraj <aswinrajan6969@gmail.com>
2021-10-20 09:52:23 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+// function displayValue(num){
+//     let res = document.querySelector("#result")
+//     res.value+=num
+// }
+
+// function clearBox(){
+//     result.value=""
+// }
+
+// function evaluateExpression(){
+//     let expr=result.value;
+//     let out=eval(expr)
+//     result.value=out;
+// }
+
+var displayValue=(num)=>result.value+=num
+
+var clearBox=()=>result.value=""
+
+var evaluateExpression=()=>result.value=eval(result.value)
